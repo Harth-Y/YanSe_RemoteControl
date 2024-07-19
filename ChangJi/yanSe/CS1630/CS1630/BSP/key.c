f7fa014a1693b4e05da4f732350cefd7627325d5
Harth-Y <154529992+Harth-Y@users.noreply.github.com>
2024-07-19 17:58:56 +0800

    7.19

--- ChangJi/yanSe/CS1630/CS1630/BSP/key.c
+++ ChangJi/yanSe/CS1630/CS1630/BSP/key.c
@@ -4,7 +4,8 @@
 #include "app_tx.h"
 #include "bsp_usart.h"
 
-volatile unsigned char SLEEP_STATUS = 1;
+#define UPDATE_REG(x)  __asm__("MOVR _" #x ",F")
+// volatile unsigned char SLEEP_STATUS = 1;
 
 void set_PA_low(void)
 {
@@ -24,296 +25,137 @@ void set_PB_low(void)
 void key_init(void)
 {
     // 配置PA按钮
-	IOSTA = C_PA2_Input | C_PA3_Input | C_PA4_Input | C_PA5_Input | C_PA6_Input; // 配置PA为输入
-	APHCON = 0x83; // 设置2、3、4、5、6上拉
-	delay_ms(10);
+	IOSTA = C_PA2_Input | C_PA3_Input | C_PA4_Input | C_PA5_Input | C_PA6_Input; //| C_PA7_Input;  // 配置PA2、3、4、5、6为输入
+	APHCON = 0b10100011; // 设置2、3、4、6上拉
+	PCON = 0xc8; // 设置5上拉
 
-	if(PORTAbits.PA2)
-	{
-		usart_send_string("[KEY] PA2 high\r\n");
-	}
-	else
-	{
-		usart_send_string("[KEY] PA2 low\r\n");
-	}
-	if(PORTAbits.PA3)
-	{
-		usart_send_string("[KEY] PA3 high\r\n");
-	}
-	else
-	{
-		usart_send_string("[KEY] PA3 low\r\n");
-	}
-	if(PORTAbits.PA4)
-	{
-		usart_send_string("[KEY] PA4 high\r\n");
-	}
-	else
-	{
-		usart_send_string("[KEY] PA4 low\r\n");
-	}
-	if(PORTAbits.PA5)// 此处应该是上拉才对，但是是低的
-	{
-		usart_send_string("[KEY] PA5 high\r\n");
-	}
-	else
-	{
-		usart_send_string("[KEY] PA5 low\r\n");
-	}
-	if(PORTAbits.PA6)
-	{
-		usart_send_string("[KEY] PA6 high\r\n");
-	}
-	else
-	{
-		usart_send_string("[KEY] PA6 low\r\n");
-	}
-
-
-/*
-    // 配置PB按钮
+    //配置PB按钮
 	IOSTB = C_PB0_Output | C_PB1_Output | C_PB2_Output | C_PB3_Output | C_PB4_Output; // 配置PB为输出低电平
 	set_PB_low();
 	PORTBbits.PB4 = 0;
-*/
-	IOSTB = C_PB0_Input | C_PB1_Input | C_PB2_Input | C_PB3_Input;
-	BPHCON = 0xF0;
-	if(PORTBbits.PB0)
-	{
-		usart_send_string("[KEY] PB0 high\r\n");
-	}
-	else
-	{
-		usart_send_string("[KEY] PB0 low\r\n");
-	}
-	if(PORTBbits.PB1)
-	{
-		usart_send_string("[KEY] PB1 high\r\n");
-	}
-	else
-	{
-		usart_send_string("[KEY] PB1 low\r\n");
-	}
-	if(PORTBbits.PB2)
-	{
-		usart_send_string("[KEY] PB2 high\r\n");
-	}
-	else
-	{
-		usart_send_string("[KEY] PB2 low\r\n");
-	}
-	if(PORTBbits.PB3)
-	{
-		usart_send_string("[KEY] PB3 high\r\n");
-	}
-	else
-	{
-		usart_send_string("[KEY] PB3 low\r\n");
-	}
-	uint8_t key = 0;
-	key = PORTB;
-	key = key & 0x0f;
-
-	if(key == 0x0f)
-	{
-		usart_send_string("[KEY] key = 0f\r\n");
-	}
-	else if(key == 0x14)
-	{
-		usart_send_string("[KEY] key = 14\r\n");
-	}
-	else
-	{
-		usart_send_string("[KEY] key = ?\r\n");
-	}
-
 }
 
 unsigned char Check_Keydown()
 {
-	unsigned char KeyValue=0;
+
+    //usart_send_string("[KEY] in CHECK KEY\r\n");
+    unsigned char KeyValue=0;
     unsigned char sCodeValue = 0;
-	unsigned char KeyStatus = 0;
+    unsigned char KeyStatus = 0;
 
-	KeyStatus = PORTA;
-	KeyStatus = KeyStatus & 0x7c;
+    KeyStatus = PORTA;
+    KeyStatus = KeyStatus & 0x7c;
 
-	if(KeyStatus != 0x7c)
-	{
-		delay_ms(10);
-		KeyStatus = PORTA;
+    if(KeyStatus != 0x7c)
+    {
+        delay_ms(10);
+        KeyStatus = PORTA;
         KeyStatus = KeyStatus & 0x7c;
 
-		if(KeyStatus != 0x7c)
-		{
-			usart_send_string("[KEY] key down\r\n");
-			if(!PORTAbits.PA6)
+        if(KeyStatus != 0x7c)
+        {
+			// SLEEP_STATUS = 0;
+            usart_send_string("[KEY] key down\r\n");
+            if(!PORTAbits.PA6)
+            {
+               // usart_send_string("[KEY] key.PA6 down\r\n");
+                KeyValue=0x11;
+            }
+
+            // if(!PORTAbits.PA7)
+            // {
+            //  KeyValue=0x12;
+            // }
+
+
+
+            KeyStatus = KeyStatus & 0x3c;
+			// if(KeyStatus == 0x38)
+			// {
+			// 	usart_send_string("[KEY] KeyStatus == 0x38\r\n");
+			// }
+            switch(KeyStatus)
+            {
+
+                case(0X38): KeyValue=0x01;
+							//usart_send_string("[KEY] A0111\r\n");
+							break;
+                case(0X34): KeyValue=0x02;
+							//usart_send_string("[KEY] A1011\r\n");
+							break;
+                case(0X2C): KeyValue=0x03;
+							//usart_send_string("[KEY] A1101\r\n");
+							break;
+                case(0X1C): KeyValue=0x04;
+							//usart_send_string("[KEY] A1110\r\n");
+							break;
+            }
+
+            APHCON = 0b10111111; // 2、3、4上拉取消
+            PCON = 0xe8; // 5上拉取消
+            IOSTA = 0b01000000; // 配置PA2、3、4、5为输出低电平
+            set_PA_low();
+
+            IOSTB = 0b00001111; // 配置PB0、1、2、3为输入
+            BPHCON = 0xF0; // 0、1、2、3上拉
+
+			if(0b00000111 == (PORTB & 0x0f))
 			{
-				usart_send_string("[KEY] key.PA6 down\r\n");
-				KeyValue=0x11;
+				KeyValue=KeyValue;
+				// usart_send_string("[KEY] B0111\r\n");
 			}
-
-			// if(!PORTAbits.PA7)
-			// {
-			// 	KeyValue=0x12;
-			// }
-			//SLEEP_STATUS = 0;
-			// if(PORTAbits.PA2)
-			// {
-			// 	usart_send_string("[KEY] PA2 high\r\n");
-			// }
-			// else
-			// {
-			// 	usart_send_string("[KEY] PA2 low\r\n");
-			// }
-			// if(PORTAbits.PA3)
-			// {
-			// 	usart_send_string("[KEY] PA3 high\r\n");
-			// }
-			// else
-			// {
-			// 	usart_send_string("[KEY] PA3 low\r\n");
-			// }
-			// if(PORTAbits.PA4)
-			// {
-			// 	usart_send_string("[KEY] PA4 high\r\n");
-			// }
-			// else
-			// {
-			// 	usart_send_string("[KEY] PA4 low\r\n");
-			// }
-			// if(PORTAbits.PA5)
-			// {
-			// 	usart_send_string("[KEY] PA5 high\r\n");
-			// }
-			// else
-			// {
-			// 	usart_send_string("[KEY] PA5 low\r\n");
-			// }
-			// if(PORTAbits.PA6)
-			// {
-			// 	usart_send_string("[KEY] PA6 high\r\n");
-			// }
-			// else
-			// {
-			// 	usart_send_string("[KEY] PA6 low\r\n");
-			// }
-
-			KeyStatus = KeyStatus & 0x3c;
-			switch(KeyStatus)
+			else if(0b00001011 == (PORTB & 0x0f))
 			{
-				case(0X38):	KeyValue=0x01;usart_send_string("[KEY] A1110\r\n");break;
-				case(0X34):	KeyValue=0x02;usart_send_string("[KEY] A1101\r\n");break;
-				case(0X2C): KeyValue=0x03;usart_send_string("[KEY] A1011\r\n");break;
-				case(0X1C):	KeyValue=0x04;usart_send_string("[KEY] A0111\r\n");break;
+				KeyValue=KeyValue+0x04;
+				// usart_send_string("[KEY] B1011\r\n");
 			}
-
-			usart_send_string("[KEY] keyA IN change to keyB IN\r\n");
-			APHCON = 0xFF;
-			IOSTA = C_PA2_Output | C_PA3_Output | C_PA4_Output | C_PA5_Output;
-			set_PA_low();
-
-			IOSTB = C_PB0_Input | C_PB1_Input | C_PB2_Input | C_PB3_Input;
-			BPHCON = 0xF0;
-
-			KeyStatus = PORTB;
-            KeyStatus = KeyStatus & 0x0F;
-
-			// if(PORTBbits.PB0)
-			// {
-			// 	usart_send_string("[KEY] PB0 high\r\n");
-			// }
-			// else
-			// {
-			// 	usart_send_string("[KEY] PB0 low\r\n");
-			// }
-			// if(PORTBbits.PB1)
-			// {
-			// 	usart_send_string("[KEY] PB1 high\r\n");
-			// }
-			// else
-			// {
-			// 	usart_send_string("[KEY] PB1 low\r\n");
-			// }
-			// if(PORTBbits.PB2)
-			// {
-			// 	usart_send_string("[KEY] PB2 high\r\n");
-			// }
-			// else
-			// {
-			// 	usart_send_string("[KEY] PB2 low\r\n");
-			// }
-			// if(PORTBbits.PB3)
-			// {
-			// 	usart_send_string("[KEY] PB3 high\r\n");
-			// }
-			// else
-			// {
-			// 	usart_send_string("[KEY] PB3 low\r\n");
-			// }
-
-			switch(KeyStatus)
+			else if(0b00001101 == (PORTB & 0x0f))
 			{
-				usart_send_string("[KEY] in PB switch\r\n");
-				case(0X0e):	KeyValue=KeyValue;usart_send_string("[KEY] B1110\r\n");break;
-				case(0X0d):	KeyValue=KeyValue+0x04;usart_send_string("[KEY] B1101\r\n");break;
-				case(0X0b): KeyValue=KeyValue+0x08;usart_send_string("[KEY] B1011\r\n");break;
-				case(0X07):	KeyValue=KeyValue+0x0c;usart_send_string("[KEY] B0111\r\n");break;
-				case(0x0f):	usart_send_string("[KEY] B1111\r\n");break;
+				KeyValue=KeyValue+0x08;
+				//usart_send_string("[KEY] B1101\r\n");
 			}
-			usart_send_string("[KEY] out PB switch\r\n");
-		}
-		else
-		{
-			return 0;
-		}
-		usart_send_string("[KEY] keyB IN change to keyA IN\r\n");
-		IOSTA = C_PA2_Input | C_PA3_Input | C_PA4_Input | C_PA5_Input; // 配置PA为输入
-		APHCON = 0x83; // 设置2、3、4、5、6上拉
-		BPHCON = 0x3f; // 清除所有上拉
-		IOSTB = C_PB0_Output | C_PB1_Output | C_PB2_Output | C_PB3_Output; // 配置PB为输出低电平
-		set_PB_low();
+			else if(0b00001110 == (PORTB & 0x0f))
+			{
+				KeyValue=KeyValue+0x0c;
+				//usart_send_string("[KEY] B1110\r\n");
+			}
+        }
+        else
+        {
+            return 0;
+        }
+
+        key_init();
 
         sCodeValue = KeyValue - 0x01;
+		uint8_t i = 0;
 
-
-		KeyStatus = PORTA;
-		KeyStatus = KeyStatus & 0x7c;
         while(1)     // 等待按键释放
         {
-			usart_send_string("[KEY] in while\r\n");
-			usart_send_string("[KEY] while start\r\n");
-			// 等待，准备下一次发送
-			// led
-			PORTBbits.PB4 = 1;
-			delay_250ms();
-			PORTBbits.PB4 = 0;
-			delay_250ms();
-			KeyStatus = PORTA;
-			KeyStatus = KeyStatus & 0x7c;
-
-			if(KeyStatus != 0x7c)
-
-			{
-				usart_send_string("[KEY] in while key down\r\n");
-			}
-			else
-			{
-				usart_send_string("[KEY] in while key up\r\n");
-				break;
-			}
+            // 等待，准备下一次发送
+            // led
+            PORTBbits.PB4 = 1;
+            delay_ms(100);
+            PORTBbits.PB4 = 0;
+            delay_ms(100);
             // 发送数据包
-            //send_ble_packet(sCodeValue);
-			//SLEEP_STATUS = 1;
+            send_ble_packet(sCodeValue);
+			//i++;
+			key_init();
+
+			if(PORTAbits.PA6 && PORTAbits.PA5 && PORTAbits.PA4 && PORTAbits.PA3 && PORTAbits.PA2)
+
+            {
+				//i = 0;
+                usart_send_string("[KEY] key up\r\n");
+				// SLEEP_STATUS = 1;
+                return 0;
+            }
 			PORTBbits.PB4 = 1;
-			delay_250ms();
-			PORTBbits.PB4 = 0;
-			delay_250ms();
-			usart_send_string("[KEY] while end\r\n");
+            delay_ms(100);
+            PORTBbits.PB4 = 0;
+            delay_ms(100);
         }
-		}
-	return 0;
+    }
+    return 0;
 }
-
-
-
--- ChangJi/yanSe/CS1630/CS1630/main.c
+++ ChangJi/yanSe/CS1630/CS1630/main.c
@@ -10,79 +10,87 @@
 
 #define UPDATE_REG(x)  __asm__("MOVR _" #x ",F")
 
-volatile unsigned char system_tick = 0;//系统计时变量
-extern volatile unsigned char SLEEP_STATUS;
+// volatile unsigned char system_tick = 0;//系统计时变量
+// extern volatile unsigned char SLEEP_STATUS;
 
-void isr_sleep() __interrupt(0)
+// void isr_sleep() __interrupt(0)
+// {
+//   static unsigned char tick_1s = 0;
+//   static unsigned char tick_30s = 0;
+//   if(INTFbits.T0IF)
+//   {
+//     INTFbits.T0IF = 0;//清除中断标志
+//     TMR0 = 100;//Timer0寄存器重新从100开始计数
+//     //每5ms进入中断，计数200次就是1s
+//     if(++tick_1s >= 200)
+//     {
+//       usart_send_string("[MAIN] 1s\r\n");
+//       tick_1s = 0;
+//       if(++tick_30s >= 30)
+//       {
+//         tick_30s = 0;
+//         if(SLEEP_STATUS == 1)
+//         {
+//           wake_up_init();
+//           SLEEP();
+//         }
+//       }
+//     }
+//   }
+// }
+
+void isr(void) __interrupt(0)
 {
-  static unsigned char tick_1s = 0;
-  static unsigned char tick_30s = 0;
-  if(INTFbits.T0IF)
-  {
-    INTFbits.T0IF = 0;//清除中断标志
-    TMR0 = 100;//Timer0寄存器重新从100开始计数
-    //每5ms进入中断，计数200次就是1s
-    if(++tick_1s >= 200)
+    if(INTFbits.PABIF)
     {
-      tick_1s = 0;
-      if(++tick_30s >= 30)
-      {
-        tick_30s = 0;
-        if(SLEEP_STATUS == 1)
-        {
-          SLEEP();
-        }
-      }
+      usart_send_string("[MAIN] key down it\r\n");
+
+      INTFbits.PABIF = 0;					// 清除PABIF（PortB输入变化中断标志位）
     }
-  }
 }
 
-void isr_wake_up() __interrupt(1)
-{
-  if(INTFbits.PABIF)
-  {
-    INTFbits.PABIF = 0;
-  }
-}
-
-void timer_init(void)
-{
-  PCON1 = C_TMR0_Dis;
-  TMR0 = 100;
-  T0MD = C_PS0_TMR0;
-  T0MD |= C_PS0_Div64;
-  INTE = C_INT_TMR0;
-  INTF = 0;
-}
+// void timer_init(void)
+// {
+//   PCON1 = C_TMR0_Dis;
+//   TMR0 = 100;
+//   T0MD = C_PS0_TMR0;
+//   T0MD |= C_PS0_Div64;
+//   INTE = C_INT_TMR0;
+//   INTF = 0;
+// }
 
 void wake_up_init(void)
 {
-  AWUCON = 0xC3;
-  BWUCON = 0x0f;
-  INTE |= C_INT_PABKey;
-  INTF = 0;
+  AWUCON = 0xfc;
+  BWUCON = 0x00;
+  IOSTA = C_PA2_Input | C_PA3_Input | C_PA4_Input | C_PA5_Input | C_PA6_Input; //| C_PA7_Input;  // 配置PA2、3、4、5、6为输入
+  APHCON = 0b10100011; // 设置2、3、4、6上拉
+
+  INTE = C_INT_PABKey;
+  INTF = 0x00;
 }
 
 void main(void)
 {
   DISI();
-  key_init();
   usart_init();
-  //HS6230_Init();
+  HS6230_Init();
   //timer_init();
-  //wake_up_init();
+  wake_up_init();
   ENI();
-  usart_send_string("[MAIN] start\r\n");
-  PORTBbits.PB4 = 1;
-  delay_250ms();
-  PORTBbits.PB4 = 0;
-  delay_250ms();
-  PORTBbits.PB4 = 1;
-  delay_250ms();
-  PORTBbits.PB4 = 0;
-  delay_250ms();
+
   while (1)
   {
-    //Check_Keydown();
+    DISI();
+    key_init();
+    Check_Keydown();
+    wake_up_init();
+    UPDATE_REG(PORTA);
+    INTF = 0x00;
+    usart_send_string("[MAIN] sleep\r\n");
+    ENI();
+    SLEEP();
+    INTFbits.PABIF = 0;	// 清除PABIF（PortB输入变化中断标志位）
+    usart_send_string("[MAIN] wake up\r\n");
 	}
 }
